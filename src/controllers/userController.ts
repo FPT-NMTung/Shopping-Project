2ea435375f56d460e8df7942c4bf3c8322b6ee41
NMTung <nmtung.study@gmail.com>
2022-01-11 16:17:14 +0700

    Update API active user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,6 +3,9 @@ import bcrypt from 'bcrypt'
 import User from '../models/user'
 import jwt from 'jsonwebtoken'
 import verifyInput from '../common/commonFunction'
+import templateActiveCode from '../common/templateActiveCode'
+import SendMail from '../config/mailManager'
+import user from '../models/user'
 
 class UserController {
   public static signUp = async (req: Request, res: Response): Promise<Response> => {
@@ -14,7 +17,7 @@ class UserController {
       type: 'string',
       required: true,
       minLength: 5,
-      maxLength: 300,
+      maxLength: 300
     })
     password = <string>verifyInput({
       input: password,
@@ -38,8 +41,30 @@ class UserController {
       })
     }
 
+    const randomActiveCode = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    const activeCode = Array.from(Array(8).keys()).map(i => randomActiveCode[Math.floor(Math.random() * randomActiveCode.length)]).join(
+      '')
+
     const hashPassword = await bcrypt.hash(password, 12)
-    await User.createUser(email.split('@')[0], email, hashPassword)
+    await User.createUser(email.split('@')[0], email, hashPassword, activeCode)
+
+    // send email
+    const content = {
+      to: [email],
+      from: 'nmtung.temp@gmail.com',
+      fromname: 'Shopping | Project', // We set the `fromname` parameter here
+      subject: 'Active code | Shopping',
+      text: 'Active code | Shopping',
+      html: templateActiveCode(email, activeCode)
+    }
+
+    SendMail.sendMail(content, function (err, a) {
+      if (err) {
+        res.status(500).json({
+          message: 'Internal server error'
+        })
+      }
+    })
 
     return res.status(200).json({
       message: 'User created'
@@ -55,7 +80,7 @@ class UserController {
       type: 'string',
       required: true,
       minLength: 5,
-      maxLength: 300,
+      maxLength: 300
     })
     password = <string>verifyInput({
       input: password,
@@ -89,12 +114,66 @@ class UserController {
     const token = jwt.sign({
       id: searchUser[0].id,
       email: searchUser[0].email,
-      name: searchUser[0].name,
+      name: searchUser[0].name
     }, 'nmtungofficial')
 
     return res.status(200).json({
       message: 'Login success',
       token: token
+    })
+  }
+
+  public static active = async (req: Request, res: Response) => {
+    let userId = req.body.userId as number
+    let activeCode = req.body.code as string
+
+    userId = <number>verifyInput({
+      input: userId,
+      type: 'number',
+      required: true,
+    })
+    activeCode = <string>verifyInput({
+      input: activeCode,
+      type: 'string',
+      required: true,
+      minLength: 8,
+      maxLength: 8,
+    })
+
+    if (userId === null || activeCode === null) {
+      return res.status(400).json({
+        message: 'Invalid input'
+      })
+    }
+
+    const [searchUser] = await User.getById(userId)
+    if (searchUser.length === 0) {
+      return res.status(404).json({
+        message: 'User not found'
+      })
+    }
+
+    if (searchUser[0].codeActive === null) {
+      return res.status(409).json({
+        message: 'User already active'
+      })
+    }
+
+    if (searchUser[0].codeActive !== activeCode) {
+      return res.status(401).json({
+        message: 'Invalid active code'
+      })
+    }
+
+    if (searchUser[0].expCodeActive < Date.now()) {
+      return res.status(401).json({
+        message: 'Active code expired'
+      })
+    }
+
+    await User.active(userId)
+    return res.status(200).json({
+      message: 'Active success'
     })
   }
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -13,12 +13,22 @@ class User {
     return db.execute('select * from user where email = ?', [email]) as Promise<RowDataPacket[]>
   }
 
-  public static createUser(name: string, email: string, password: string): Promise<RowDataPacket[]> {
+  public static getById(id: number): Promise<RowDataPacket[]> {
+    return db.execute('select * from user where id = ?', [id]) as Promise<RowDataPacket[]>
+  }
+
+  public static createUser(name: string, email: string, password: string, activeCode: string): Promise<RowDataPacket[]> {
     const createdAt = new Date()
     const updatedAt = new Date()
+    // after 15 minutes, the active code will be expired
+    const expiredAt = new Date(createdAt.getTime() + 15 * 60 * 1000)
 
-    return db.execute('insert into user (email, password, name, createdAt, updatedAt) values (?, ?, ?, ?, ?)',
-      [email, password, name, createdAt, updatedAt]) as Promise<RowDataPacket[]>
+    return db.execute('insert into user (email, password, name, createdAt, updatedAt, codeActive, expCodeActive) values (?, ?, ?, ?, ?, ?, ?)',
+      [email, password, name, createdAt, updatedAt, activeCode, expiredAt]) as Promise<RowDataPacket[]>
+  }
+
+  public static active(id: number) {
+    return db.execute('update user set codeActive = NULL, expCodeActive = NULL where id = ?', [id]) as Promise<RowDataPacket[]>
   }
 }
 
