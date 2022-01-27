32ebb3b706d67f5ddc84c3528b35cd1560fb3b26
NMTung <nmtung.study@gmail.com>
2022-01-28 01:48:51 +0700

    Add API get all and delete for address

--- src/routers/addressRouter.ts
+++ src/routers/addressRouter.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import AddressController from '../controllers/addressController'
+import AuthorizationRouter from '../middlewares/Authorization'
 const router = express.Router()
 
 router.get('/address/province', AddressController.getProvince)
@@ -7,5 +8,9 @@ router.get('/address/province', AddressController.getProvince)
 router.get('/address/district', AddressController.getDistrict)
 
 router.get('/address/ward', AddressController.getWard)
+
+router.get('/address/get-all', AuthorizationRouter, AddressController.getAllAddress)
+
+router.delete('/address/delete', AuthorizationRouter, AddressController.deleteAddress)
 
 export default router
